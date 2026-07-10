83666119549916180b2e1fc774a5c87674a2fbed
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-07-10 17:46:17 +0530

    for loop practice implemented

---
+++ src/forloop/practice.java
@@ -0,0 +1,17 @@
+package forloop;
+
+public class practice {
+	public static void main(String[] args) {
+		
+		for(int i = 0 ; i <= 10 ; i++) {
+			System.out.println(i);
+			
+		}
+		
+		System.out.println("____________________");
+		for(int i = 10 ; i >= 0 ; i--) {
+			System.out.println(i);
+		}
+	}
+
+}
