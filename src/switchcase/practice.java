d8afa3c407cc6ad0cc688f3532d1473e41af3ab2
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-07-10 17:41:29 +0530

    switch case practice implemented

---
+++ src/switchcase/practice.java
@@ -0,0 +1,30 @@
+package switchcase;
+
+public class practice{
+	public static void main(String[] args) {
+		
+		int m = 2;
+		
+		switch(m) {
+		
+		case 1:
+			System.out.println("red");
+			break;
+			
+		case 2:
+			System.out.println("blue");
+			break;
+			
+		case 3:
+			System.out.println("green");
+			break;
+			
+		default:
+			System.out.println("please enter proper number");
+		
+		}
+		
+		
+	}
+
+}
