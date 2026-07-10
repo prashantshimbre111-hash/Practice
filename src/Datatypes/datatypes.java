014514b8b47503c580e8fe3fcdd1086e28eda5a9
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-07-10 16:23:15 +0530

    datatypes practice implemented

---
+++ src/Datatypes/datatypes.java
@@ -0,0 +1,25 @@
+package Datatypes;
+
+public class datatypes {
+	
+	public static void main(String[] args) {
+		
+		int a = 12;
+		double d = 12.43;
+		short s = 1234;
+		long k = 12345678945678l;
+		char c = 'a';
+		boolean b = true;
+		float f = 12f;
+		
+		System.out.println(a);
+		System.out.println(d);
+		System.out.println(s);
+		System.out.println(k);
+		System.out.println(c);
+		System.out.println(b);
+		System.out.println(f);
+		
+	}
+
+}
