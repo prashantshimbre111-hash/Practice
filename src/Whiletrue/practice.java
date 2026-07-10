a8f7a5b9280694965436886fa837986b351a41c6
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-07-10 18:02:34 +0530

    while true and break practice implemented

---
+++ src/Whiletrue/practice.java
@@ -0,0 +1,18 @@
+package Whiletrue;
+
+public class practice {
+	
+	public static void main(String[] args) {
+		
+		int i = 0;
+		while(true) {
+			System.out.println(i);
+			i++;
+			
+			if(i==100) {
+				break;
+			}
+		}
+	}
+
+}
