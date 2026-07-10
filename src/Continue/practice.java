85366ff77cf7d10c5b6c58c0022fbe863380ed95
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-07-10 17:59:15 +0530

    continue statement practice implemented

---
+++ src/Continue/practice.java
@@ -0,0 +1,16 @@
+package Continue;
+
+public class practice {
+	
+	public static void main(String[] args) {
+		
+		for(int i = 0; i <= 10 ; i++) {
+			
+			if(i == 5) {
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+
+}
