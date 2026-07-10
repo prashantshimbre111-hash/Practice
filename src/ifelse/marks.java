d685799d26a48f55ebd4c0cb9d7e07ffecab953c
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-07-10 17:34:32 +0530

    if else practice implemented

---
+++ src/ifelse/marks.java
@@ -0,0 +1,17 @@
+package ifelse;
+
+public class marks {
+	
+	public static void main(String[] args) {
+		
+		int m = 99;
+		
+	if(m>=35) {
+		System.out.println("pass");}
+	
+	else {
+		System.out.println("fail");
+	}
+
+}
+}
