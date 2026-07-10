f110a7aae20e55160fec890dac41532325d1610f
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-07-10 17:52:16 +0530

    while loop practice implemented

---
+++ src/whileloops/practice.java
@@ -0,0 +1,24 @@
+package whileloops;
+
+public class practice {
+
+	public static void main(String[] args) {
+
+		int i = 0;
+		while (i <= 50) {
+
+			System.out.println(i);
+			i++;
+		}
+		
+		System.out.println("-------------------");
+
+		int j = 50;
+		while (j >= 0) {
+			System.out.println(j);
+			j--;
+		}
+
+	}
+
+}
