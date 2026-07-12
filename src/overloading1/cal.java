541de78736ff9401ed31ef36af9e4f783f829196
prashantshimbare111 <prashantshimbre111@gmail.com>
2026-07-12 16:26:13 +0530

    overloading/compile time polymorphism practice implemented

---
+++ src/overloading1/cal.java
@@ -0,0 +1,13 @@
+package overloading1;
+
+public class cal {
+	
+	public void sum(int a, int b) {
+		System.out.println(a+b);
+	}
+	
+	public void sum(int a, int b,int c){
+		System.out.println(a+b+c);
+	}
+
+}
---
+++ src/overloading1/test.java
@@ -0,0 +1,10 @@
+package overloading1;
+
+public class test {
+	
+	public static void main(String[] args) {
+		cal c = new cal();
+		c.sum(1, 1, 2);
+	}
+
+}
